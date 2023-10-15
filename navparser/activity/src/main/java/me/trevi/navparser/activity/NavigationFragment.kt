8a7b060329316485d54214305fb7f035de7ba667
Mukil-Krishna <m.k.balamurugan@student.tue.nl>
2023-10-15 15:39:21 +0200

    bit code for directions

--- navparser/activity/src/main/java/me/trevi/navparser/activity/NavigationFragment.kt
+++ navparser/activity/src/main/java/me/trevi/navparser/activity/NavigationFragment.kt
@@ -104,6 +104,14 @@ class NavigationFragment : Fragment() {
             //3) then come here and use that check sum to figure out left, right, straight,
             // if ( md5CheckSum.equals("13e68aacc62531a385e2b3e9705e0701")) then straight;
             // etc
+            // there are two different images for straight thus the or statement
+            // if ( md5CheckSum.equals("7c2731606742cb8545a39dea66ba445e") || md5CheckSum.equals("b7e506e2329811258c5912fca19b3d8f")) then straight;
+            // there are two types of right (slight right and right) thus the or statement
+            // if ( md5CheckSum.equals("79c23f3c035d18baf29ab5078c387963") || md5CheckSum.equals("ca7fe0fa11d1e40a70df0fb4f69d3a92")) then right;
+            // same thing for left
+            // if ( md5CheckSum.equals("3f2e712ee53137133c147218bddfa5e4") || md5CheckSum.equals("1efb6b1cc749733fa6a5e7a21a597e55")) then left;
+            // after the map takes the last turn it shows a symbol for destination but i believe this symbol can be ignored;
+            // if ( md5CheckSum.equals("0fa51ebacc6511870edde8a6a481382e")) then destination;
         }
 
     }
